e9abff1d1f6f88763e37ba423ee7dd2966a8a180
José Ignacio Digrazia <josedigrazia7@gmail.com>
2022-09-19 19:44:53 -0300

    Initial commit

---
+++ src/main/java/com/example/ejercicio4/condicionalIf.java
@@ -0,0 +1,26 @@
+package com.example.ejercicio4;
+
+
+public class condicionalIf {
+    public static void main(String[] args) {
+
+        double numeroIf = 1.5;
+
+        if (numeroIf > 0)
+
+            System.out.println("La variable " + numeroIf + " es positiva");
+        if (numeroIf < 0)
+            System.out.println("La variable " + numeroIf + " es negativa");
+
+        if (numeroIf == 0)
+            System.out.println("La variable " + numeroIf + " es cero");
+    }
+
+
+
+
+    }
+    }
+
+
+
