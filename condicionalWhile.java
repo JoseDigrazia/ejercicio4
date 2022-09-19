feb67df800defd1508ab99a1e05e50a6b06e75e6
JoseDigrazia <92757132+JoseDigrazia@users.noreply.github.com>
2022-09-19 20:43:30 -0300

    Add files via upload

---
+++ condicionalWhile.java
@@ -0,0 +1,14 @@
+package com.example.demo2;
+
+public class condicionalWhile {
+
+    public static void main(String[] args) {
+                int numeroWhile = 0;
+
+                while(numeroWhile<3) {
+                    numeroWhile = numeroWhile + 1;
+                    System.out.println(numeroWhile);
+                }
+
+            }
+        }
