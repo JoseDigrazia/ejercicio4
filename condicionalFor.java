380147192f389fa91cfcb362b19169e4db9e81e3
JoseDigrazia <92757132+JoseDigrazia@users.noreply.github.com>
2022-09-19 21:06:35 -0300

    Add files via upload

---
+++ condicionalFor.java
@@ -0,0 +1,13 @@
+package com.example.demo2;
+
+public class condicionalFor{
+
+    public static void main(String[] args) {
+
+        for(int numeroFor=0; numeroFor<=3; numeroFor = numeroFor+1 ){
+            System.out.println(numeroFor);
+        }
+
+
+    }
+}
