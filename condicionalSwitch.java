6da9e51ea734298b094c3829b1a7a800c4d40ff7
JoseDigrazia <92757132+JoseDigrazia@users.noreply.github.com>
2022-09-19 21:13:33 -0300

    Add files via upload

---
+++ condicionalSwitch.java
@@ -0,0 +1,40 @@
+package com.example.demo2;
+
+import java.util.Scanner;
+
+public class condicionalSwitch{
+
+    public static void main(String[] args) {
+//variables
+        int numero;
+        String estacion="";
+        //entrada
+        Scanner objeto=new Scanner(System.in);
+        System.out.println("Ingrese el número de la estación: ");
+        numero=objeto.nextInt();
+        //proceso
+        switch(numero){
+            case 1:
+                estacion="Verano";
+                break;
+            case 2:
+                estacion="Otoño";
+                break;
+            case 3:
+                estacion="Invierno";
+                break;
+            case 4:
+                estacion="Primavera";
+                break;
+            default:
+                estacion="No especifica";
+                break;
+        }
+        //salida
+        System.out.println("");
+        System.out.println("---Resultado---");
+        System.out.println("La estación del año es: "+estacion);
+
+    }
+
+}
